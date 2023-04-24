dc9ba10419fd8478f9a23fca0d04cbefae0718f5
Nathan Jaouen <NathanJaouen@eurofins.com>
2023-04-24 15:05:24 +0200

    Add test and reorganization

---
+++ src/TfcToChrTransformer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TfcToChrTransformer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
