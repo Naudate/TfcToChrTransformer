454d481991eed872617d59f11e342f1dd75329ae
Nathan Jaouen <NathanJaouen@eurofins.com>
2023-04-25 09:23:34 +0200

    add log capacity

--- src/TfcToChrTransformer/Program.cs
+++ src/TfcToChrTransformer/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace TfcToChrTransformer
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.LayoutRenderers;
+using NLog.Web;
+using System;
+
+namespace TfcToChrTransformer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // NLog: setup the logger first to catch all errors
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("init main - application starting...");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()
+                .UseStartup<Startup>();
     }
 }
