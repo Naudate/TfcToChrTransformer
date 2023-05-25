d40409c23a668deee1dd9ec859d5f80dd68b3451
Nathan Jaouen <NathanJaouen@eurofins.com>
2023-05-25 15:52:11 +0200

    xmltocsv

--- src/TfcToChrTransformer/Program.cs
+++ src/TfcToChrTransformer/Program.cs
@@ -3,6 +3,7 @@ using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using NLog;
 using NLog.Web;
+using System.Text;
 using System.Xml;
 
 namespace TfcToChrTransformer
@@ -25,89 +26,11 @@ namespace TfcToChrTransformer
                 Directory.CreateDirectory(outputFolder);
 
                 // Construct the full path to the file
-                string xlsxFilePath = Path.Combine(outputFolder, "output.xlsx");
+                string csvFilePath = Path.Combine(outputFolder, "output.csv");
 
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.Load(xmlFilePath);
+                XmlToCsvConverter converter = new XmlToCsvConverter();
 
-                var program = new Program();
-
-                // Get all distinct tag names from the XML document
-                HashSet<string> tagNames = program.GetDistinctTagNames(xmlDocument);
-
-                HashSet<string> list = program.GetDistinct(xmlDocument);
-
-                // Create the XLSX document
-                using (SpreadsheetDocument document = SpreadsheetDocument.Create(xlsxFilePath, SpreadsheetDocumentType.Workbook))
-                {
-                    // Add a workbook part to the document
-                    WorkbookPart workbookPart = document.AddWorkbookPart();
-                    workbookPart.Workbook = new Workbook();
-
-                    // Add a worksheet part to the workbook part
-                    WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
-                    worksheetPart.Worksheet = new Worksheet(new SheetData());
-
-                    // Add the worksheet part to the workbook
-                    Sheets sheets = document.WorkbookPart!.Workbook.AppendChild(new Sheets());
-                    Sheet sheet = new Sheet() { Id = document.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet1" };
-                    sheets.Append(sheet);
-
-                    // Get the sheet data
-                    SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>()!;
-
-                    // Create the header row with XML tags
-                    Row headerRow = new Row();
-
-                    // Add tag names as headers
-                    foreach (string tagName in tagNames)
-                    {
-                        headerRow.Append(CreateCell(tagName));
-                    }
-
-                    // Add the header row to the sheet data
-                    sheetData.Append(headerRow);
-
-                    logger.Info("Headers have been created");
-
-                    var rootNode = xmlDocument.DocumentElement;
-                    var shop = rootNode?.SelectNodes("//SHOPITEM");
-                    uint count = 2;
-                    foreach (XmlNode? shopItemNode in shop!)
-                    {
-                        Row valueRow = new Row() { RowIndex = count };
-                        foreach (var item in list)
-                        {
-                            var index = tagNames.ToList().IndexOf(item);
-                            var columnName = GetColumnName(index + 1);
-                            columnName += count;
-                            logger.Info(columnName);
-                            valueRow.Append(program.CreateCellWithColumn(shopItemNode?.SelectSingleNode(item)?.InnerText!, columnName));
-                        }
-
-                        var paramNode = shopItemNode?.SelectSingleNode("PARAMETERS");
-                        foreach (XmlNode parameter in paramNode?.SelectNodes("Parameter")!)
-                        {
-                            string paramName = parameter.SelectSingleNode("ParamName")!.InnerText;
-                            string paramValue = parameter.SelectSingleNode("ParamValue")!.InnerText;
-                            string paramUnit = parameter.SelectSingleNode("ParamUnit")!.InnerText;
-                            var index = tagNames.ToList().IndexOf(paramName);
-                            var columnName = GetColumnName(index);
-                            columnName += count;
-                            valueRow.Append(program.CreateCellWithColumn($"{paramValue} {paramUnit}", columnName));
-                        }
-                        
-                        sheetData.Append(valueRow);
-                        count++;
-                    }                    
-
-                    // Save the workbook
-                    workbookPart.Workbook.Save();
-
-                    // Close the document
-                    document.Dispose();
-                }
-
+                converter.ConvertXmlToCsv(xmlFilePath, csvFilePath);
 
                 logger.Info("File created !");
             }
@@ -122,91 +45,6 @@ namespace TfcToChrTransformer
                 // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
                 LogManager.Shutdown();
             }
-        }
-
-        private HashSet<string> GetDistinctTagNames(XmlDocument xmlDocument)
-        {
-            HashSet<string> tagNames = new HashSet<string>();
-
-            // Get all elements in the XML document
-            XmlNodeList elements = xmlDocument.GetElementsByTagName("*");
-
-            // Iterate over the elements and add the tag names to the set
-            foreach (XmlNode element in elements)
-            {
-                
-                if (element.Name.Equals("ParamName"))
-                {
-                    tagNames.Add(element.InnerText);
-                }
-                else
-                {
-                    if (element.Name != "Parameter" && element.Name != "PARAMETERS" && element.Name != "ParamValue" && element.Name != "ParamUnit" && element.Name != "SHOP" && element.Name != "SHOPITEM")
-                    {
-                        tagNames.Add(element.Name);
-                    }
-                }
-            }
-
-            return tagNames;
-        }
-
-        private HashSet<string> GetDistinct(XmlDocument xmlDocument)
-        {
-            HashSet<string> tagNames = new HashSet<string>();
-
-            // Get all elements in the XML document
-            XmlNodeList elements = xmlDocument.GetElementsByTagName("*");
-
-            // Iterate over the elements and add the tag names to the set
-            foreach (XmlNode element in elements)
-            {
-                if (element.Name != "Parameter" && element.Name != "PARAMETERS" && element.Name != "ParamValue" && element.Name != "ParamName" && element.Name != "ParamUnit" && element.Name != "SHOP" && element.Name != "SHOPITEM")
-                {
-                    tagNames.Add(element.Name);
-                }
-            }
-
-            return tagNames;
-        }
-
-        private static Cell CreateCell(string xmlTag)
-        {
-            Cell cell = new Cell();
-            cell.DataType = CellValues.String;
-
-            // Add the XML tag as the value of the cell
-            cell.CellValue = new CellValue(xmlTag);
-
-            return cell;
-        }
-
-        // Helper method to get the column name from the column index
-        private static string GetColumnName(int columnIndex)
-        {
-            string columnName = string.Empty;
-            int dividend = columnIndex;
-
-            while (dividend > 0)
-            {
-                int modulo = (dividend - 1) % 26;
-                columnName = Convert.ToChar('A' + modulo) + columnName;
-                dividend = (dividend - modulo) / 26;
-            }
-
-            return columnName;
-        }
-
-        private Cell CreateCellWithColumn(string tagName, string columnName)
-        {
-            Cell cell = new Cell();
-            cell.DataType = CellValues.String;
-            cell.CellReference = columnName;
-
-            // Add the XML tag as the value of the cell
-            cell.CellValue = new CellValue(tagName);
-
-            return cell;
         }
     }
 }
---
+++ src/TfcToChrTransformer/XmlToCsvConverter.cs
@@ -0,0 +1,102 @@
+﻿using System.Xml;
+using System.Text;
+
+public class XmlToCsvConverter
+{
+    public void ConvertXmlToCsv(string xmlFilePath, string csvFilePath)
+    {
+        // Load the XML document
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.Load(xmlFilePath);
+
+        // Get the root element
+        XmlElement root = xmlDocument.DocumentElement;
+
+        HashSet<string> tagNames = GetDistinctTagNames(xmlDocument);
+
+        // Create a StringBuilder to store the CSV content
+        StringBuilder csvContent = new StringBuilder();
+
+        // Get the column headers from the XML
+        foreach (string tagName in tagNames)
+        {
+            csvContent.Append(tagName + ";");
+        }
+        csvContent.AppendLine();
+
+        XmlNodeList dataRows = root.ChildNodes;
+        // Get the data rows from the XML
+        foreach (XmlNode row in dataRows)
+        {
+            string[] Line = new string[tagNames.Count];
+            for (int i = 0; i < Line.Length; i++)
+            {
+                Line[i] = ";";
+            }
+            int count = 0;
+            XmlNodeList rowData = row.ChildNodes;
+            foreach (XmlNode data in rowData)
+            {
+                if (data.Name != "PARAMETERS")
+                {
+                    if (count == tagNames.ToList().IndexOf(data.Name))
+                    {
+                        Line[count] = data.InnerXml.Replace(";",",") + ";";
+                    }
+                }
+                count++;
+            }
+
+            var paramNode = row?.SelectSingleNode("PARAMETERS");
+            foreach (XmlNode parameter in paramNode?.SelectNodes("Parameter")!)
+            {
+                string paramName = parameter.SelectSingleNode("ParamName")!.InnerText;
+                string paramValue = parameter.SelectSingleNode("ParamValue")!.InnerText;
+                string paramUnit = parameter.SelectSingleNode("ParamUnit")!.InnerText;
+                var index = tagNames.ToList().IndexOf(paramName.Replace(";", ","));
+                Line[index] = paramValue.Replace(";", ",") + paramUnit.Replace(";", ",") + ";";
+            }
+
+            csvContent.Append(string.Join("" , Line));
+            csvContent.AppendLine();
+        }
+
+        ClearCsv(csvFilePath);
+
+        // Write the CSV content to a file
+        File.WriteAllText(csvFilePath, csvContent.ToString(), Encoding.UTF8);
+    }
+
+    private static HashSet<string> GetDistinctTagNames(XmlDocument xmlDocument)
+    {
+        HashSet<string> tagNames = new HashSet<string>();
+
+        // Get all elements in the XML document
+        XmlNodeList elements = xmlDocument.GetElementsByTagName("*");
+
+        // Iterate over the elements and add the tag names to the set
+        foreach (XmlNode element in elements)
+        {
+
+            if (element.Name.Equals("ParamName"))
+            {
+                tagNames.Add(element.InnerText);
+            }
+            else
+            {
+                if (element.Name != "Parameter" && element.Name != "PARAMETERS" && element.Name != "ParamValue" && element.Name != "ParamUnit" && element.Name != "SHOP" && element.Name != "SHOPITEM")
+                {
+                    tagNames.Add(element.Name);
+                }
+            }
+        }
+
+        return tagNames;
+    }
+
+    public static void ClearCsv(string csvFilePath)
+    {
+        // Clear the contents of the CSV file by overwriting it with an empty string
+        File.WriteAllText(csvFilePath, string.Empty);
+    }
+}
